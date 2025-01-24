83aacfc5e94d20240c1116dd5eb2ac42d889b2f2
mojhos <s.mojtaba.h65@gmail.com>
2025-01-24 10:47:59 -0500

    install first ec2 on aws and update gitignore

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+resource "aws_instance" "Test" {
+  ami           = "ami-0df8c184d5f6ae949"
+  instance_type = "t2.micro"
+  key_name      = "A4L"
+  tags = {
+    name       = "test-ec2"
+    Created_By = "terraform"
   }
-}
-# a new line of code
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
 }
