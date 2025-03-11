31b71f6b902548a9cb08c9d2a9fd6cdf3cfda713
mojhos <s.mojtaba.h65@gmail.com>
2025-03-11 10:06:29 -0400

    add eks with terraform

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "Test" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  tags = {
-    Name       = var.Name
-    Created_By = var.Created_By
-  }
-}
